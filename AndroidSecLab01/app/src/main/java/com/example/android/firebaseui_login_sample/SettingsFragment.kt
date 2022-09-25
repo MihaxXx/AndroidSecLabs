6ba11a940cd7e541e188db65cb8719929723063f
Mikhail <mikes22@yandex.ru>
2022-09-26 00:43:03 +0300

    Lab01: Add settings

--- AndroidSecLab01/app/src/main/java/com/example/android/firebaseui_login_sample/SettingsFragment.kt
+++ AndroidSecLab01/app/src/main/java/com/example/android/firebaseui_login_sample/SettingsFragment.kt
@@ -21,6 +21,7 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
 
@@ -39,5 +40,19 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> Log.i(TAG, "Authenticated")
+                // If the user is not logged in, they should not be able to set any preferences,
+                // so navigate them to the login fragment
+                LoginViewModel.AuthenticationState.UNAUTHENTICATED -> navController.navigate(
+                    R.id.loginFragment
+                )
+                else -> Log.e(
+                    TAG, "New $authenticationState state that doesn't require any UI change"
+                )
+            }
+        })
     }
 }
